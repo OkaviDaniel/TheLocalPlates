d2a129e8a195ca3a3733ab2c927224835a5973df
Daniel Okavi <74859686+OkaviDaniel@users.noreply.github.com>
2023-05-10 20:14:28 +0300

    ShoppingCart

--- app/src/main/java/com/example/thelocalplates8/ShoppingCartActivity.java
+++ app/src/main/java/com/example/thelocalplates8/ShoppingCartActivity.java
@@ -2,6 +2,7 @@ package com.example.thelocalplates8;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,44 +13,65 @@ import java.util.List;
 
 public class ShoppingCartActivity extends AppCompatActivity {
 
-    private LinearLayout cartItemsTextView;
+    private LinearLayout cartItemsLayout;
+    private TextView totalPriceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
 
-        cartItemsTextView = findViewById(R.id.cartItemsLayout);
+        cartItemsLayout = findViewById(R.id.cartItemsLayout);
+        totalPriceTextView = findViewById(R.id.totalPriceTextView);
+
         updateCartItems();
     }
 
     private void updateCartItems() {
         // Retrieve the cart items from your data source
-        List<String> cartItems = new ArrayList<>();
-        // Example of adding an item to the cart
-        String itemToAdd = "Product XYZ";
-        cartItems.add(itemToAdd);
-        cartItems.add("Hello");
-        cartItems.add("Michael");
-        // You can use a List or any other data structure to store the items
+        List<Pair<String, Integer>> cartItems = new ArrayList<>();
+        // Example of adding items to the cart with quantities
+        cartItems.add(new Pair<>("Product XYZ", 2)); // Product XYZ with quantity 2
+        cartItems.add(new Pair<>("Hello", 1)); // Hello with quantity 1
+        cartItems.add(new Pair<>("Michael", 3)); // Michael with quantity 3
 
-        // Assuming you have a List<String> cartItems
-        StringBuilder sb = new StringBuilder();
-        for (String item : cartItems) {
-            sb.append(item).append("\n");
-        }
+        double totalPrice = 0.0;
+        for (Pair<String, Integer> cartItem : cartItems) {
+            String item = cartItem.first;
+            int quantity = cartItem.second;
 
-        for (String item : cartItems) {
+            // Assuming you have a method to retrieve the price for each item
+            double itemPrice = getItemPrice(item);
+            double totalPriceForItem = itemPrice * quantity;
+            totalPrice += totalPriceForItem;
+
             TextView textView = new TextView(this);
             textView.setLayoutParams(new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
             ));
-            textView.setText(item);
+            textView.setText(item + " (Quantity: " + quantity + ") - $" + totalPriceForItem);
             textView.setTextSize(16);
             textView.setPadding(16, 8, 16, 8);
 
-            cartItemsTextView.addView(textView);
+            cartItemsLayout.addView(textView);
+        }
+
+        // Display the total price
+        totalPriceTextView.setText("Total Price: $" + totalPrice);
+    }
+
+    private double getItemPrice(String item) {
+        // Replace this with your logic to retrieve the price for each item
+        // Example: Assuming the item price is based on a fixed value per item
+        if (item.equals("Product XYZ")) {
+            return 10.99;
+        } else if (item.equals("Hello")) {
+            return 5.99;
+        } else if (item.equals("Michael")) {
+            return 7.99;
+        } else {
+            return 0.0;
         }
     }
 }
