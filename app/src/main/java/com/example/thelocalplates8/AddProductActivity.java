1b7febf984c5e998d7fb59699a307ebcd469f991
Daniel Okavi <74859686+OkaviDaniel@users.noreply.github.com>
2023-05-22 16:28:51 +0300

    Select Image and upload Image

--- app/src/main/java/com/example/thelocalplates8/AddProductActivity.java
+++ app/src/main/java/com/example/thelocalplates8/AddProductActivity.java
@@ -1,19 +1,45 @@
 package com.example.thelocalplates8;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.thelocalplates8.Controllers.ProductController;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddProductActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Uri imageUri;
+    private StorageReference storageReference;
+    private String fileName;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +56,27 @@ public class AddProductActivity extends AppCompatActivity {
         Button selectImageButton = (Button) findViewById(R.id.buttonSelectProductImage);
         Button uploadImageButton = (Button) findViewById(R.id.buttonUploadProductImage);
         Button addProductButton = (Button) findViewById(R.id.buttonAddProduct);
+        imageView = (ImageView) findViewById(R.id.imageViewProduct);
+
 
 
         SharedPreferences sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
         String businessId = sp.getString("businessId", "");
 
+
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        uploadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
 
         // Need to think about how to upload an image
 
@@ -63,4 +105,61 @@ public class AddProductActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    private void selectImage(){
+        Intent intent = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        onActivityResultLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> onActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        imageUri = data.getData();
+                        imageView.setImageURI(imageUri);
+                    }
+                }
+            });
+
+    private void uploadImage(){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading file..");
+        progressDialog.show();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.ENGLISH);
+        Date now = new Date();
+        fileName = formatter.format(now);
+
+        SharedPreferences sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+        String userId = sp.getString("userId", "");
+        Log.d("Add product screen", "UserId is: " + userId);
+
+        storageReference = FirebaseStorage.getInstance().getReference("images/"+userId+"/Products/"+fileName);
+        storageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(AddProductActivity.this, "Successfully uploaded!", Toast.LENGTH_SHORT).show();
+                if(progressDialog.isShowing()){
+                    progressDialog.dismiss();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AddProductActivity.this, "Failed to upload!", Toast.LENGTH_SHORT).show();
+                if(progressDialog.isShowing()){
+                    progressDialog.dismiss();
+                }
+            }
+        });
+
+    }
+
+
 }
